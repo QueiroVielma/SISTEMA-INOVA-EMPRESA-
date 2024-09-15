e4cde2ed64fc253fc14f130ef16ac14d5e196287
Mamonha <mamonha302@gmail.com>
2024-09-14 22:32:29 -0300

    Fixing syntax user type enum (EVALUATOR/COLLABORATOR)

--- src/main/java/inovaEmpresa/enums/UserType.java
+++ src/main/java/inovaEmpresa/enums/UserType.java
@@ -8,17 +8,26 @@ import lombok.Getter;
 
 public enum UserType {
     ADMIN(1),
-    evaluators(2),
-    collaborators(3);
+    EVALUATOR(2),
+    COLLABORATOR(3);
 
-    private final int velue;
+    private final int value;
 
-    public static UserType fromVelue(int value){
+    public static UserType fromValue(int value){
         for (UserType userType : UserType.values()){
-            if (userType.velue==value){
+            if (userType.value == value){
                 return userType;
             }
         }
         throw  new IllegalArgumentException("invalid priority value"+ value);
+    }
+
+    public static UserType fromString(String type) {
+        for (UserType userType : UserType.values()) {
+            if (userType.name().equalsIgnoreCase(type)) {
+                return userType;
+            }
+        }
+        throw new IllegalArgumentException("Invalid user type: " + type);
     }
 }
