f0a3404d07369005140562e7cabffd5038a6905e
Mamonha <mamonha302@gmail.com>
2024-09-15 00:39:34 -0300

    Creating idea dto

---
+++ src/main/java/inovaEmpresa/dto/idea/CreateIdea.java
@@ -0,0 +1,30 @@
+package inovaEmpresa.dto.idea;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CreateIdea {
+    private Long userId;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Impact is required")
+    private String impact;
+
+    private Double estimatedCost;
+
+    @NotBlank(message = "Description is required")
+    @Size(max = 1000, message = "Description must be up to 1000 characters")
+    private String description;
+
+    private Double score;
+}
