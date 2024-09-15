3d35a4d2ab6c6f9faa3428729e500214df30c8d1
Mamonha <mamonha302@gmail.com>
2024-09-15 00:40:07 -0300

    Creating store/assign methods

--- src/main/java/inovaEmpresa/controllers/EventController.java
+++ src/main/java/inovaEmpresa/controllers/EventController.java
@@ -1,7 +1,38 @@
 package inovaEmpresa.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import inovaEmpresa.dto.event.AssignEvaluators;
+import inovaEmpresa.dto.event.CreateEvent;
+import inovaEmpresa.entities.Event;
+import inovaEmpresa.services.EventService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@RequestMapping("api/events")
 public class EventController {
+
+    @Autowired
+    protected EventService eventService;
+    @PostMapping
+    public ResponseEntity<?> store(@Validated @RequestBody CreateEvent data){
+        try{
+          Event createdUser = eventService.store(data);
+          return ResponseEntity.ok(createdUser);
+        }catch (ResponseStatusException e){
+            return ResponseEntity.status(e.getStatusCode()).body(e.getReason());
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+        }
+    }
+
+    @PutMapping("/assign")
+    public ResponseEntity<Void> assign(@RequestBody AssignEvaluators data) {
+        eventService.assignEvents(data);
+        return ResponseEntity.ok().build();
+    }
 }
