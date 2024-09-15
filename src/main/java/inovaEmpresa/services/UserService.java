c96983bcc77402bf15584031ec134d99ac6100a7
Mamonha <mamonha302@gmail.com>
2024-09-15 00:41:46 -0300

    Adjusting updateUserStatus

--- src/main/java/inovaEmpresa/services/UserService.java
+++ src/main/java/inovaEmpresa/services/UserService.java
@@ -32,14 +32,11 @@ public class UserService {
     }
 
     public User updateUserStatus(Long id, UpdateUserType userUpdateDTO) {
-        User user = userRepository.findById(id).orElseThrow(() ->
-                new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
-
         User userLogged = userRepository.findById(userUpdateDTO.getUserId()).orElseThrow(() ->
                 new ResponseStatusException(HttpStatus.NOT_FOUND, "Logged user not found"));
-
         if (UserPolicy.canUpdateUser(userLogged)) {
-
+            User user = userRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
             if (userUpdateDTO.getUserType() != null) {
                 UserType userType = UserType.fromString(userUpdateDTO.getUserType().name());
                 user.setType(userType.getValue());
