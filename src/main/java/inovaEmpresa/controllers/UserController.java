042e774370961dfb0b420c9f8ec09899b4bcae4d
Mamonha <mamonha302@gmail.com>
2024-09-14 22:30:17 -0300

    Creating user service/controller

---
+++ src/main/java/inovaEmpresa/controllers/UserController.java
@@ -0,0 +1,39 @@
+package inovaEmpresa.controllers;
+
+import inovaEmpresa.dto.user.UpdateUserType;
+import inovaEmpresa.dto.user.UserDTO;
+import inovaEmpresa.entities.User;
+import inovaEmpresa.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/collaborators")
+    public ResponseEntity<User> registerCollaborator(@RequestBody UserDTO userDTO) {
+        User createdUser = userService.registerCollaborator(userDTO);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    @PutMapping("/type/{id}")
+    public ResponseEntity<?> updateStatus(@PathVariable Long id, @Validated @RequestBody UpdateUserType userUpdateDTO) {
+        try {
+            User updatedUser = userService.updateUserStatus(id, userUpdateDTO);
+            return ResponseEntity.ok(updatedUser);
+        } catch (ResponseStatusException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(e.getReason());
+        } catch (Exception e) {
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+        }
+    }
+}
---
+++ src/main/java/inovaEmpresa/services/UserService.java
@@ -0,0 +1,52 @@
+package inovaEmpresa.services;
+
+import inovaEmpresa.dto.user.UpdateUserType;
+import inovaEmpresa.dto.user.UserDTO;
+import inovaEmpresa.entities.User;
+import inovaEmpresa.enums.UserType;
+import inovaEmpresa.policies.UserPolicy;
+import inovaEmpresa.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerCollaborator(UserDTO userDTO) {
+        User user = new User();
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        user.setType(UserType.COLLABORATOR.getValue());
+
+        return userRepository.save(user);
+    }
+
+    public User updateUserStatus(Long id, UpdateUserType userUpdateDTO) {
+        User user = userRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        User userLogged = userRepository.findById(userUpdateDTO.getUserId()).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Logged user not found"));
+
+        if (UserPolicy.canUpdateUser(userLogged)) {
+
+            if (userUpdateDTO.getUserType() != null) {
+                UserType userType = UserType.fromString(userUpdateDTO.getUserType().name());
+                user.setType(userType.getValue());
+            }
+            return userRepository.save(user);
+        } else {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Usuario não é um admin");
+        }
+    }
+}
