d0c93b4e11b0c6a8222e4d08aa207e8f59472b71
Mamonha <mamonha302@gmail.com>
2024-09-15 11:04:12 -0300

    Adding no args constructor

--- src/main/java/inovaEmpresa/dto/user/UpdateUserType.java
+++ src/main/java/inovaEmpresa/dto/user/UpdateUserType.java
@@ -3,11 +3,13 @@ package inovaEmpresa.dto.user;
 import inovaEmpresa.enums.UserType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateUserType {
     private Long userId;
     private UserType userType;
