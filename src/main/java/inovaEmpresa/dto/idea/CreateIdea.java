44f114d1dc51686764f80a6ba856c898003fb7a7
Mamonha <mamonha302@gmail.com>
2024-09-15 16:35:17 -0300

    Adding no args constructor

--- src/main/java/inovaEmpresa/dto/idea/CreateIdea.java
+++ src/main/java/inovaEmpresa/dto/idea/CreateIdea.java
@@ -6,11 +6,13 @@ import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateIdea {
     private Long userId;
 
