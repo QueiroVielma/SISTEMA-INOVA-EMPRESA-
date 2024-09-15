2307cc53678c028adf22d5e71ae1cc5429efc707
Mamonha <mamonha302@gmail.com>
2024-09-15 00:43:34 -0300

    Adding queries

--- src/main/java/inovaEmpresa/repositories/IdeaRepository.java
+++ src/main/java/inovaEmpresa/repositories/IdeaRepository.java
@@ -2,6 +2,14 @@ package inovaEmpresa.repositories;
 
 import inovaEmpresa.entities.Idea;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface IdeaRepository  extends JpaRepository<Idea, Long> {
+    Optional<Idea> findByUserId(Long userId);
+
+    @Query("SELECT i FROM Idea i ORDER BY i.score DESC")
+    List<Idea> findTop10Ideas();
 }
